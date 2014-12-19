332a6ae64a7b0295309d8062a3ff9fc6bdd36f7b
mason <masonmullendore@gmail.com>
2014-12-18 20:41:23 -0800

    Added server and js
    node server. Toggle nav and resize for mobile. Dropdown hover

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+	$('.icon-bars').on('click', function(){
+		$('nav ul:nth-child(3)').toggle();
+		$(this).toggleClass('active');
+	});
+
+	$(window).on('resize', function(){
+		if( $(window).width() > 768 && $('nav ul:nth-child(3)').css({"display":"none"}) ){
+			$('nav ul:nth-child(3)').css({
+				"display": 'inline-block'
+			});
+		}
+	});
+
 });
